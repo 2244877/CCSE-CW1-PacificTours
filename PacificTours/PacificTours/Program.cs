2b1be90fe992012ada1253eccb4891e40c7f5661
2244877 <leo.stirzaker@warwick.ac.uk>
2024-01-15 00:12:11 +0000

    Added extra fields in registration section
    The fields of FirstName, LastName, Phone Number, Address have been addded with some additional formatting

--- PacificTours/PacificTours/Program.cs
+++ PacificTours/PacificTours/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using PacificTours.Services;
+using Microsoft.AspNetCore.Identity;
+using PacificTours.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +13,8 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
     options.UseSqlServer(connectionString);
 });
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false).AddEntityFrameworkStores<ApplicationDbContext>();
 
 var app = builder.Build();
 
--- PacificTours/PacificTours/Services/ApplicationDbContext.cs
+++ PacificTours/PacificTours/Services/ApplicationDbContext.cs
@@ -1,13 +1,30 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using PacificTours.Models;
 
 namespace PacificTours.Services
 {
-    public class ApplicationDbContext: IdentityDbContext
+    public class ApplicationDbContext: IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions options) : base(options) // Constructor for class
         {
 
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var admin = new IdentityRole("admin");
+            admin.NormalizedName = "admin";
+
+            var client = new IdentityRole("client");
+            client.NormalizedName = "client";
+
+            var seller = new IdentityRole("seller");
+            seller.NormalizedName = "seller";
+
+            builder.Entity<IdentityRole>().HasData(admin, client, seller);
         }
     }
 }
