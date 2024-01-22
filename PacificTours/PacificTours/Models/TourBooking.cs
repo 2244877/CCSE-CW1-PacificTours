667d2121a972fefdd220fa5f01a160da89fa01c6
2244877 <leo.stirzaker@warwick.ac.uk>
2024-01-22 21:46:55 +0000

    Added TourBooking model

---
+++ PacificTours/PacificTours/Models/TourBooking.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PacificTours.Models
+{
+    public class TourBooking
+    {
+        [Key]
+        public int TourBooking_Id { get; set; }
+        public string User_Id { get; set; }
+        [Required]
+        public int Tour_Id { get; set; }
+        public virtual Tour? Tour { get; set; }
+        [Required]
+        public DateTime CheckInDate { get; set; }
+    }
+}
