365a97fe3e349a802fe912039267fee01b0c941c
2244877 <leo.stirzaker@warwick.ac.uk>
2024-01-22 20:47:42 +0000

    Added the table to display all of the bookings so that the manager can search though them

---
+++ PacificTours/PacificTours/Pages/EditHotelBooking.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PacificTours.Pages
+{
+    public class EditHotelBookingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
