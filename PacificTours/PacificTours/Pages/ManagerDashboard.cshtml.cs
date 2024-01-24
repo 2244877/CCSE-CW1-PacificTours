4ac6bff31443638bface45360272f2a5f5f0f256
2244877 <leo.stirzaker@warwick.ac.uk>
2024-01-24 12:20:36 +0000

    Added hotel bookings to manager dashboard

--- PacificTours/PacificTours/Pages/ManagerDashboard.cshtml.cs
+++ PacificTours/PacificTours/Pages/ManagerDashboard.cshtml.cs
@@ -1,14 +1,32 @@
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PacificTours.Models;
+using PacificTours.Services;
 
 namespace PacificTours.Pages
 {
     [Authorize(Roles = "admin")]
     public class ManagerDashboardModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public ManagerDashboardModel(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public ApplicationUser user { get; set; }
+
+        public IList<HotelBooking> HotelBookingList { get; set; }
+        public async Task OnGet()
+        {
+            HotelBookingList = await _context.hotelbookings
+                .ToListAsync();
+            user = await _userManager.GetUserAsync(User);
         }
     }
 }
